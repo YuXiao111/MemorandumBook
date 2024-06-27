527cbb66541edfe962b2c7aeaa91edb924819521
YuXiao <YuXiao_0111@163.com>
2024-06-27 21:17:02 +0800

    240627

---
+++ LoveNote.Shell/ShellModule.cs
@@ -0,0 +1,21 @@
+﻿using LoveNote.Shell.ViewModels;
+using LoveNote.Shell.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TookKit.Mvvm.Interfaces;
+
+namespace LoveNote.Shell
+{
+    public class ShellModule : IModule
+    {
+        public void RegisterTypes(IContainer container)
+        {
+            //container.Register<ShellView>();
+            //container.Register<ShellViewModel>();
+
+        }
+    }
+}
